ce33df4e2848a57df8f0a04fce2ad99e1c19d7fa
Servet <srvtclk@yahoo.com>
2025-02-13 23:24:52 +0100

    Updated models

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'locations';
+
+    protected $fillable = [
+        'name',
+        'address',
+        'max_pallet',
+    ];
+
+    // One Location has Many Pallets
+    public function pallets()
+    {
+        return $this->hasMany(Pallet::class, 'location_id', 'id');
+    }
 }
--- app/Models/Pallet.php
+++ app/Models/Pallet.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pallet extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'pallets'; // Matches the database table name
+
+    protected $fillable = [
+        'amount',
+        'location_id',
+        'product_id',
+    ];
+
+    // Many Pallets belong to One Location
+    public function location()
+    {
+        return $this->belongsTo(Location::class, 'location_id', 'id');
+    }
+
+    // One Pallet has One Product
+    public function product()
+    {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
 }
